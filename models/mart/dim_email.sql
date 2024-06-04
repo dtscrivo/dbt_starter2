ffa664653ca9cfe334325b2ecde937107f3600b9
dtscrivo <dtscrivo@gmail.com>
2024-06-03 20:32:59 -0700

    dim_email revert=

--- models/mart/dim_email.sql
+++ models/mart/dim_email.sql
@@ -1,27 +1,25 @@
--- models/contact_model.sql
-
 {{ config(materialized='table') }}
 
 WITH contact AS (
   SELECT
-    id AS contact_id,
-    DATE(property_createdate) AS property_createdate,
-    analytics.fnEmail(property_email) AS property_email,
-    analytics.fnEmail(property_email_2) AS property_email_2,
-    analytics.fnEmail(property_hs_additional_emails) AS property_hs_additional_emails,
+    id as contact_id,
+    date(property_createdate) as property_createdate,
+    analytics.fnEmail(property_email) as property_email,
+    analytics.fnEmail(property_email_2) as property_email_2,
+    analytics.fnEmail(property_hs_additional_emails) as property_hs_additional_emails,
     property_email_status,
     is_deleted
   FROM
-    {{ ref('hubspot2_contact') }}
+    `bbg-platform.hubspot2.contact`
 )
 
 SELECT
-  COALESCE(a.contact_id, b.contact_id, c.contact_id) AS contact_id,
-  COALESCE(a.property_createdate, b.property_createdate, c.property_createdate) AS dt,
-  COALESCE(a.property_email, b.property_hs_additional_emails, c.property_email_2) AS email_all,
-  COALESCE(a.property_email, b.property_email, c.property_email) AS email_prime,
-  COALESCE(a.property_email_status, b.property_email_status, c.property_email_status) AS status_all,
-  COALESCE(a.is_deleted, b.is_deleted, c.is_deleted) AS is_deleted_all
+  coalesce(a.contact_id, b.contact_id, c.contact_id) as contact_id,
+  coalesce(a.property_createdate, b.property_createdate, c.property_createdate) as dt,
+  coalesce(a.property_email, b.property_hs_additional_emails, c.property_email_2) as email_all,
+  coalesce(a.property_email, b.property_email, c.property_email) AS email_prime,
+  coalesce(a.property_email_status, b.property_email_status, c.property_email_status) as status_all,
+  coalesce(a.is_deleted, b.is_deleted, c.is_deleted) as is_deleted_all
 FROM
   contact a
 FULL OUTER JOIN
@@ -32,5 +30,4 @@ FULL OUTER JOIN
   contact c
 ON
   a.property_email = c.property_email_2
-WHERE
-  COALESCE(a.property_email, b.property_hs_additional_emails, c.property_email_2) IS NOT NULL
+WHERE coalesce(a.property_email, b.property_hs_additional_emails, c.property_email_2) is not null
