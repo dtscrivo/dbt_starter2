25ae45c6b26be1e71e75697aed762dcded2a1f43
dtscrivo <dtscrivo@gmail.com>
2024-08-06 15:29:34 -0700

    events tracking

---
+++ models/mart/fct_hs_events.sql
@@ -0,0 +1,45 @@
+{{ config(materialized='table') }}
+
+WITH events AS (
+    SELECT 
+        property_email,
+        MAX(case when name like "%Phoenix%" and name like "Attended%" then 1 else 0 end) as phoenix_attended,
+        MAX(case when name like "%Phoenix%" and name like "Registered%" then 1 else 0 end) as phoenix_registered,
+        MAX(case when name like "%DC%" and name like "Attended%" then 1 else 0 end) as dc_attended,
+        MAX(case when name like "%DC%" and name like "Registered%" then 1 else 0 end) as dc_registered,
+        MAX(case when name like "%Atlanta%" and name like "Attended%" then 1 else 0 end) as atlanta_attended,
+        MAX(case when name like "%Atlanta%" and name like "Registered%" then 1 else 0 end) as atlanta_registered,
+        MAX(case when name like "%Philadelphia%" and name like "Attended%" then 1 else 0 end) as philadelphia_attended,
+        MAX(case when name like "%Philadelphia%" and name like "Registered%" then 1 else 0 end) as philadelphia_registered,
+        MAX(case when name like "%Dallas%" and name like "Attended%" then 1 else 0 end) as dallas_attended,
+        MAX(case when name like "%Dallas%" and name like "Registered%" then 1 else 0 end) as dallas_registered,
+        MAX(case when name like "%San Antonio%" and name like "Attended%" then 1 else 0 end) as san_antonio_attended,
+        MAX(case when name like "%San Antonio%" and name like "Registered%" then 1 else 0 end) as san_antonio_registered,
+        MAX(case when name like "%Baltimore%" and name like "Attended%" then 1 else 0 end) as baltimore_attended,
+        MAX(case when name like "%Baltimore%" and name like "Registered%" then 1 else 0 end) as baltimore_registered,
+        MAX(case when name like "%Tampa%" and name like "Attended%" then 1 else 0 end) as tampa_attended,
+        MAX(case when name like "%Tampa%" and name like "Registered%" then 1 else 0 end) as tampa_registered,
+        MAX(case when name like "%Charlotte%" and name like "Attended%" then 1 else 0 end) as charlotte_attended,
+        MAX(case when name like "%Charlotte%" and name like "Registered%" then 1 else 0 end) as charlotte_registered,
+        MAX(case when name like "%Raleigh%" and name like "Attended%" then 1 else 0 end) as raleigh_attended,
+        MAX(case when name like "%Raleigh%" and name like "Registered%" then 1 else 0 end) as raleigh_registered,
+        MAX(case when name like "%Greensboro%" and name like "Attended%" then 1 else 0 end) as greensboro_attended,
+        MAX(case when name like "%Greensboro%" and name like "Registered%" then 1 else 0 end) as greensboro_registered,
+        MAX(case when name like "%Greensboro%" and name like "Invited%" then 1 else 0 end) as greensboro_invited,
+        MAX(case when name like "%Fayetteville%" and name like "Attended%" then 1 else 0 end) as fayetteville_attended,
+        MAX(case when name like "%Fayetteville%" and name like "Registered%" then 1 else 0 end) as fayetteville_registered,
+        MAX(case when name like "%Fayetteville%" and name like "Invited%" then 1 else 0 end) as fayetteville_invited,
+        MAX(case when name like "%In-Person%" and name like "Attended%" then 1 else 0 end) as inperson_attended,
+        MAX(case when name like "%In-Person%" and name like "Registered%" then 1 else 0 end) as inperson_registered
+    FROM 
+        `bbg-platform.hubspot2.contact_list` l
+    LEFT JOIN 
+        `bbg-platform.hubspot2.contact_list_member` m ON l.id = m.contact_list_id
+    LEFT JOIN 
+        `bbg-platform.hubspot2.contact` c ON m.contact_id = c.id
+    GROUP BY 
+        property_email
+)
+SELECT *
+FROM events
+--WHERE property_email = 'klara.nemes@gmail.com'
