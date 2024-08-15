d646a84732aad51e723bdb41dc125f44f84002b6
dtscrivo <dtscrivo@gmail.com>
2024-08-14 21:05:57 -0700

    invoice_generated

--- models/mart/fct_invoice_generated.sql
+++ models/mart/fct_invoice_generated.sql
@@ -1,30 +1,36 @@
 {{ config(materialized='table') }}
 
-WITH initial_payments AS (
+with initial_payments AS (
     SELECT
         analytics.fnEmail(email) as email,
         id_customer,
         id_price,
-        date(date_invoice) AS initial_date,
+        date(date_pi_created) AS date_invoice,
         case when id_price IN ('MBA_pif_inpersonpackage_5997', 'bf22') then 1 else SAFE_CAST(plan_type AS INT64) end AS plan_type -- Using SAFE_CAST to avoid errors
         , amount_collected
         , name_product
     FROM
-        `dbt_tscrivo.fct_invoice_mindmint`
+        `dbt_tscrivo.fct_deal_payments`
     WHERE
         num_payment = 1
         AND SAFE_CAST(plan_type AS INT64) IS NOT NULL -- Ensuring only valid integer values
         AND name_product not like "%@%"
-),
-generated_payments AS (
+)
+
+
+
+
+
+
+, generated_payments AS (
     SELECT
         email,
         id_customer,
         id_price,
-        initial_date,
+        date_invoice,
         plan_type,
         -- Generate each payment date using the SEQUENCE function
-        DATE_ADD(initial_date, INTERVAL n MONTH) AS payment_date,
+        DATE_ADD(date_invoice, INTERVAL n MONTH) AS payment_date,
         n + 1 AS payment_number
         , amount_collected
         , name_product
@@ -44,9 +50,13 @@ SELECT
     , 1 as is_generated
 FROM
     generated_payments
-   -- where email = 'egyn98grl@hotmail.com'
+ --   where email = 'adiyb@adiybmuhammad.com'
+ where id_price = 'taa_pp_lp_935_7'
 
 ORDER BY
     email,
     id_price,
     payment_date
+
+
+
