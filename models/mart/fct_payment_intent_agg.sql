da9951815a3842248c402dc0cd20e1e5e020db1a
dtscrivo <dtscrivo@gmail.com>
2024-08-28 01:26:41 -0700

    fct_payment_intent_agg

---
+++ models/mart/fct_payment_intent_agg.sql
@@ -0,0 +1,230 @@
+{{ config(materialized='table') }}
+
+  WITH mindmint_charge AS (
+  SELECT c.payment_intent_id
+  , DATETIME(c.created, 'America/Phoenix') as date_charge
+  , c.status
+  , row_number() over (partition by c.payment_intent_id order by c.created asc) as charge_num
+  , row_number() over (partition by c.payment_intent_id order by c.created desc) as charge_success_num
+  , c.id
+  , c.outcome_reason
+  , json_extract_scalar(c.metadata, "$.processor") as processor
+  , statement_descriptor
+  , c.metadata
+  , json_extract_scalar(c.metadata, "$.product_id") as object_id
+  , refunded
+  , coalesce(cast(m.deal_id as string), json_extract_scalar(c.metadata, "$.deal_id")) as deal_id
+  , DATETIME(r.created, 'America/Phoenix') as date_refund
+  , r.amount/100 as amount_refund
+  , c.amount/100 as amount_charge
+  , case when c.status = 'succeeded' then c.amount/100 else null end as amount_collected
+  FROM `bbg-platform.stripe_mindmint.charge` c
+  LEFT JOIN `bbg-platform.hubspot2.merged_deal` m
+    on cast(json_extract_scalar(c.metadata, "$.deal_id") as string) = cast(m.merged_deal_id as string)
+  LEFT JOIN `bbg-platform.stripe_mindmint.refund` r
+    on c.id = r.charge_id
+
+  -- WHERE c.status = "succeeded"
+--WHERE payment_intent_id = "pi_3Ma1jwISjDEJDDVR15x1pyVV"
+-- where json_extract_scalar(c.metadata, "$.product_id") is not null
+qualify row_number() over (partition by c.payment_intent_id order by c.created desc) = 1
+  )
+
+  , mastermind_charge AS (
+SELECT c.payment_intent_id
+  , DATETIME(c.created, 'America/Phoenix') as date_charge
+  , c.status
+  , row_number() over (partition by c.payment_intent_id order by c.created asc) as charge_num
+  , row_number() over (partition by c.payment_intent_id order by c.created desc) as charge_success_num
+  , c.id
+  , c.outcome_reason
+  , json_extract_scalar(c.metadata, "$.processor") as processor
+  , statement_descriptor
+  , c.metadata
+  , json_extract_scalar(c.metadata, "$.product_id") as object_id
+  , refunded
+  , coalesce(cast(m.deal_id as string), json_extract_scalar(c.metadata, "$.deal_id")) as deal_id
+  , DATETIME(r.created, 'America/Phoenix') as date_refund
+  , r.amount/100 as amount_refund
+  , c.amount/100 as amount_charge
+  , case when c.status = 'succeeded' then c.amount/100 else null end as amount_collected
+  FROM `bbg-platform.stripe_mastermind.charge` c
+  LEFT JOIN `bbg-platform.hubspot2.merged_deal` m
+    on cast(json_extract_scalar(c.metadata, "$.deal_id") as string) = cast(m.merged_deal_id as string)
+  LEFT JOIN `bbg-platform.stripe_mastermind.refund` r
+    on c.id = r.charge_id
+  -- WHERE c.status = "succeeded"
+--WHERE payment_intent_id = "pi_3Ma1jwISjDEJDDVR15x1pyVV"
+qualify row_number() over (partition by c.payment_intent_id order by c.created desc) = 1
+  )
+
+
+, mindmint_subs as (
+  SELECT s.id
+  , DATETIME(s.created, 'America/Phoenix') as date_sub_created
+  , s.status
+  , json_extract_scalar(s.metadata, "$.netsuite_CF_funnel_id") as funnel_id
+  , json_extract_scalar(s.metadata, "$.netsuite_CF_funnel_name") as funnel_name
+FROM `bbg-platform.stripe_mindmint.subscription_history` s
+where cast(_fivetran_end as string) LIKE "9999%"
+)
+
+, mastermind_subs as (
+  SELECT s.id
+  , DATETIME(s.created, 'America/Phoenix') as date_sub_created
+  , s.status
+  , json_extract_scalar(s.metadata, "$.netsuite_CF_funnel_id") as funnel_id
+  , json_extract_scalar(s.metadata, "$.netsuite_CF_funnel_name") as funnel_name
+FROM `bbg-platform.stripe_mastermind.subscription_history` s
+where cast(_fivetran_end as string) LIKE "9999%"
+)
+
+
+
+
+-- , mastermind AS (
+  SELECT
+    pi.id AS id_payment_intent,
+    pi.amount/100 as amount_pi,
+    case when c.status = 'succeeded' then c.amount_charge else null end as amount_collected,
+    c.date_charge,
+    -- case when c.status = 'succeeded' then c.amount_refund else null end as amount_refund,
+    c.amount_refund,
+    c.date_refund,
+    pi.customer_id AS id_customer,
+    pi.description,
+    pi.payment_method_id AS id_payment_method_id,
+    pi.status AS status_payment_intent,
+    pi.statement_descriptor,
+    DATETIME(pi.created, 'America/Phoenix') AS date_pi_created,
+    c.charge_num,
+    c.charge_success_num,
+    c.status AS status_charge,
+    i.status AS status_invoice,
+    CASE
+      WHEN c.status = "succeeded" AND charge_num = 1 THEN 'success'
+      ELSE 'fail'
+    END AS first_payment,
+    CASE
+      WHEN c.status IS NULL THEN "no-charge"
+      WHEN c.status = "succeeded" AND charge_num = 1 THEN 'first_charge_success'
+      WHEN c.status = "succeeded" AND charge_num != 1 THEN 'fail_recovered'
+      ELSE "fail_not_recovered"
+    END AS category,
+    i.subscription_id AS id_subscription_invoice,
+    analytics.fnEmail(cu.email) as email,
+    cu.name,
+    i.id AS id_invoice,
+
+    c.id AS id_charge,
+    i.hosted_invoice_url,
+    i.invoice_pdf,
+    il.price_id AS price_id,
+    pr.unit_amount / 100 AS price,
+    p.name AS product,
+    p.id AS id_product,
+    s.status AS status_subscription,
+    -- date(s.date_sub_created) as date_sub_created,
+    c.outcome_reason,
+    pr.recurring_interval,
+    s.id as id_subscription,
+    -- DATETIME(i.next_payment_attempt, 'America/Phoenix') AS next_attempt
+  case when c.status = 'succeeded' then (dense_rank() over(partition by email, p.id order by pi.created)) else null end as num_payment,
+  case when c.status = 'succeeded' then (dense_rank() over(partition by email, p.id order by pi.created desc)) else null end as recency,
+  dense_rank() over(partition by email, p.id, c.status order by pi.created) as num_attempt
+  , "MM" as stripe_account
+  , hp.property_hs_folder_name as group_product
+  , DATETIME(property_closedate, 'America/Phoenix') as date_closed
+  , ps.label as pipeline_stage
+  FROM `bbg-platform.stripe_mastermind.payment_intent` pi
+  LEFT JOIN mastermind_charge c ON pi.id = c.payment_intent_id
+  LEFT JOIN `bbg-platform.stripe_mastermind.invoice` i ON pi.id = i.payment_intent_id
+  LEFT JOIN `bbg-platform.stripe_mastermind.invoice_line_item` il ON i.id = il.invoice_id
+  LEFT JOIN `bbg-platform.stripe_mastermind.price` pr ON il.price_id = pr.id
+  LEFT JOIN `bbg-platform.stripe_mastermind.product` p ON pr.product_id = p.id
+  LEFT JOIN `bbg-platform.stripe_mastermind.customer` cu ON pi.customer_id = cu.id
+  LEFT JOIN mastermind_subs s ON i.subscription_id = s.id
+  LEFT JOIN `hubspot2.product` hp
+    on p.id = cast(hp.property_product_id as string)
+  LEFT JOIN `hubspot2.deal` d
+    on c.deal_id = cast(d.deal_id as string)
+LEFT JOIN `bbg-platform.hubspot2.deal_pipeline_stage` ps
+  ON cast(d.deal_pipeline_stage_id as string) = ps.stage_id
+
+  WHERE TRUE
+    and analytics.fnEmail_IsTest(cu.email) = false
+
+
+UNION ALL
+
+
+-- mindmint AS (
+
+  SELECT
+    pi.id AS id_payment_intent,
+    pi.amount/100 as amount_pi,
+    case when c.status = 'succeeded' then c.amount_charge else null end as amount_collected,
+    c.date_charge,
+    -- case when c.status = 'succeeded' then c.amount_refund else null end as amount_refund,
+    c.amount_refund,
+    c.date_refund,
+    pi.customer_id AS id_customer,
+    pi.description,
+    pi.payment_method_id AS id_payment_method_id,
+    pi.status AS status_payment_intent,
+    pi.statement_descriptor,
+    DATETIME(pi.created, 'America/Phoenix') AS date_pi_created,
+    c.charge_num,
+    c.charge_success_num,
+    c.status AS status_charge,
+    "no invoice" AS status_invoice,
+    CASE
+      WHEN c.status = "succeeded" AND charge_num = 1 THEN 'success'
+      ELSE 'fail'
+    END AS first_payment,
+    CASE
+      WHEN c.status IS NULL THEN "no-charge"
+      WHEN c.status = "succeeded" AND charge_num = 1 THEN 'first_charge_success'
+      WHEN c.status = "succeeded" AND charge_num != 1 THEN 'fail_recovered'
+      ELSE "fail_not_recovered"
+    END AS category,
+    "no invoice" AS id_subscription_invoice,
+    analytics.fnEmail(cu.email) as email,
+    cu.name,
+    "no invoice" AS id_invoice,
+    c.id AS id_charge,
+    "no invoice" as hosted_invoice_url ,
+    "no invoice" as invoice_pdf,
+    p.property_pricing_id AS price_id,
+    p.property_price AS price,
+    p.property_name AS product,
+    p.property_product_id AS id_product,
+    "no sub" AS status_subscription,
+    -- date('9999-12-31') as date_sub_created,
+    c.outcome_reason,
+    property_recurringbillingfrequency as recurring_interval,
+    "no sub" as id_subscription,
+    -- "DATETIME(i.next_payment_attempt, 'America/Phoenix')" AS next_attempt
+  case when c.status = 'succeeded' then (dense_rank() over(partition by email, c.object_id, c.status order by pi.created)) else null end as num_payment,
+  case when c.status = 'succeeded' then (dense_rank() over(partition by email, c.object_id, c.status order by pi.created desc)) else null end as recency,
+  dense_rank() over(partition by email, p.property_product_id order by pi.created) as num_attempt
+  , "BBG" as stripe_account
+  , p.property_hs_folder_name as group_product
+  , DATETIME(d.property_closedate, 'America/Phoenix') as date_closed
+  , ps.label as pipeline_stage
+  FROM `bbg-platform.stripe_mindmint.payment_intent` pi
+  LEFT JOIN mindmint_charge c ON pi.id = c.payment_intent_id
+  -- LEFT JOIN `bbg-platform.stripe_mindmint.invoice` i ON pi.id = i.payment_intent_id
+  -- LEFT JOIN `bbg-platform.stripe_mindmint.invoice_line_item` il ON i.id = il.invoice_id
+  -- LEFT JOIN `bbg-platform.stripe_mindmint.price` pr ON il.price_id = pr.id
+  -- LEFT JOIN `bbg-platform.stripe_mindmint.product` p ON pr.product_id = p.id
+  LEFT JOIN `bbg-platform.stripe_mindmint.customer` cu ON pi.customer_id = cu.id
+  -- LEFT JOIN mindmint_subs s ON i.subscription_id = s.id
+  LEFT JOIN `hubspot2.product` p
+    on c.object_id = cast(p.id as string)
+  LEFT JOIN `hubspot2.deal` d
+    on c.deal_id = cast(d.deal_id as string)
+  LEFT JOIN `bbg-platform.hubspot2.deal_pipeline_stage` ps
+  ON cast(d.deal_pipeline_stage_id as string) = ps.stage_id
+  WHERE TRUE
+    and analytics.fnEmail_IsTest(cu.email) = false
