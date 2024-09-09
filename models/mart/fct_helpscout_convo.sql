c7197a372a22e6e6bbd43911c2fbb22ba453e13b
dtscrivo <dtscrivo@gmail.com>
2024-09-08 19:26:31 -0700

    update

--- models/mart/fct_helpscout_convo.sql
+++ models/mart/fct_helpscout_convo.sql
@@ -157,7 +157,7 @@ select --c.*,
   , case when source_type = 'beacon-v2' then 1 else 0 end as is_beacon 
   , m.mailbox
   , cu.email_customer
-  , case when cu.email_customer = 'postmaster@outlook.com' or cu.email_customer = 'support@gohighlevelassist.freshdesk.com' or cu.email_customer like '%@replies.mastermind.com' or cu.email_customer like "%@deangraziosi.com" or cu.email_customer like "%@mastermind.com" then 1 else 0 end as is_notification
+  , case when cu.email_customer = 'quarantine@ess.barracudanetworks.com' or cu.email_customer like "%no-reply%" or cu.email_customer like "%do_not_reply%" or cu.email_customer = 'postmaster@outlook.com' or cu.email_customer = 'support@gohighlevelassist.freshdesk.com' or cu.email_customer like '%@replies.mastermind.com' or cu.email_customer like "%@deangraziosi.com" or cu.email_customer like "%@mastermind.com" then 1 else 0 end as is_notification
 from `bbg-platform.helpscout.conversation_history` c
 -- left join thread bc
 --   on c.id = bc.conversation_id
