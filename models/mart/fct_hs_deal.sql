31a3bf17876415abbfd54a42bbe011536d63283d
dtscrivo <dtscrivo@gmail.com>
2024-09-09 16:03:11 -0700

    deal update

--- models/mart/fct_hs_deal.sql
+++ models/mart/fct_hs_deal.sql
@@ -343,7 +343,8 @@ SELECT *
          else null end as workshop
 
 
-  , case when id_owner in (597349110,626163393,980534468,597349111,626162146,1018920904,1018935239,1211781353,143772806,1332303547,1066123713,746781400,1376975821,1332303546,2027067907,1153145590,1222041825,1278796899,1300856784,1696602043,285218002,285218005,411194086,493792977,746781399,767527108) OR
+  , --case when id_owner in (597349110,626163393,980534468,597349111,626162146,1018920904,1018935239,1211781353,143772806,1332303547,1066123713,746781400,1376975821,1332303546,2027067907,1153145590,1222041825,1278796899,1300856784,1696602043,285218002,285218005,411194086,493792977,746781399,767527108) 
+    --OR
    (case when id_owner = 1426033370 AND (date(date_closed) > date('2024-08-23') AND date(date_closed) < date('2024-08-28')) then "Charlotte"
          when id_owner = 1426033370 AND (date(date_closed) > date('2024-08-16') AND date(date_closed) <= date('2024-08-22')) then "Raleigh"
          when id_owner = 1426033370 AND (date(date_closed) > date('2024-08-02') AND date(date_closed) <= date('2024-08-04')) and (name_product like "%Hybrid (PIF)%" OR
@@ -354,7 +355,8 @@ SELECT *
          when id_owner IN (980534468,1426033370) AND (date(date_closed) > date('2024-09-05') AND date(date_closed) <= date('2024-09-11') AND (name_product like "%Mastermind Business Academy%" OR name_deal like "%The Edge (PIF) - Chad Anderson%")) then "Phoenix"
          when id_owner = 1426033370 AND (date(date_closed) > date('2024-09-19') AND date(date_closed) <= date('2024-09-22')) AND name_product like "%Mastermind Business Academy%" then "Workshop_9/20"
          when id_owner = 1426033370 AND (date(date_closed) > date('2024-10-09') AND date(date_closed) <= date('2024-10-13')) AND name_product like "%Mastermind Business Academy%" then "Workshop_10/10"
-         else null end is not null) OR
+         else null end is not null) 
+    OR
    (case when name_product LIKE "%The Action Academy%" then "TAA" 
          when name_product LIKE "%Mastermind Business Academy%" then "MBA"
          when name_product LIKE "%1:1 Coaching%" then "1:1"
@@ -364,7 +366,13 @@ SELECT *
          when name_product LIKE "%High Ticket Academy%" then "HTA"
          else "" end is not null)
          then 1 else 0 end as team_sales
-
+  ,   , case when lower(name_deal) LIKE ('%hybrid%') then "Hybrid"
+         when lower(name_deal) LIKE ('%virtual%') then "Virtual"
+         when lower(name_deal) LIKE ('%affirm%') then "Affirm"
+         when lower(name_deal) LIKE ('%lite%') then "Lite"
+         when lower(name_deal) LIKE ('%in-person%') then "In-Person"
+         when lower(name_deal) LIKE ('%Plus%') then "Plus"
+         else "Virtual" end as product_type
 
 from hubspot h
 WHERE (analytics.fnEmail_IsTest(email) = false or email = 'chrisj@remotestaff.com')
