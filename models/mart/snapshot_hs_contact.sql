55044ebbf5650f1578290e527c4d9aa49d897c99
dtscrivo <dtscrivo@gmail.com>
2024-11-19 10:20:15 -0700

    fix

--- models/mart/snapshot_hs_contact.sql
+++ models/mart/snapshot_hs_contact.sql
@@ -25,3 +25,5 @@ SELECT
     DATETIME(property_hubspot_owner_assigneddate, 'America/Phoenix') as owner_assigned_date
 FROM {{ ref('bbg-platform.hubspot2.contact') }}  -- Source table to pull device data from
 {% endsnapshot %}
+
+------
