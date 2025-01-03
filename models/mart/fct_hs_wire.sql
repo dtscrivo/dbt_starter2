9642a00b3b2d188d4a4548a07dd23fa5a890294f
dtscrivo <dtscrivo@gmail.com>
2025-01-03 13:00:10 -0700

    fix

--- models/mart/fct_hs_wire.sql
+++ models/mart/fct_hs_wire.sql
@@ -1,12 +1,21 @@
 {{ config(materialized='table') }}
 
-SELECT deal_id as id_deal
+SELECT d.deal_id as id_deal
   , sum(property_amount) as amount_hs_deal
   , property_product_name as name_product
   , analytics.fnEmail(property_email_address_of_contact) as email
   , date(property_wire_date, 'America/Phoenix') as date
   , property_total_wire_amount as amount_wire
-FROM `bbg-platform.hubspot2.deal`
+  , is_deleted
+  , property_wire_payment_
+  , e.email_prime
+FROM `bbg-platform.hubspot2.deal` d
+LEFT JOIN `bbg-platform.dbt_tscrivo.dim_email` e
+  on analytics.fnEmail(property_email_address_of_contact) = e.email_all
+LEFT JOIN `bbg-platform.hubspot2.merged_deal` m
+   on d.deal_id = m.merged_deal_id
 WHERE true
   and property_wire_payment_ = TRUE
-group by all
+--  and m.merged_deal_id IS NULL
+  and d.is_deleted = FALSE
+  group by all
