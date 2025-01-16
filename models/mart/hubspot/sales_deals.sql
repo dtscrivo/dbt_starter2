f10c18a4e80b595a8208f34d0a36bbd164622ad5
dtscrivo <dtscrivo@gmail.com>
2025-01-16 10:48:05 -0700

    update sales deals

--- models/mart/hubspot/sales_deals.sql
+++ models/mart/hubspot/sales_deals.sql
@@ -9,16 +9,18 @@ SELECT pipeline_stage
   , d.email
   , d.id_deal
   , d.name_deal
-  , l.name_closer as lead_closer
+
   , l.id_closer
   , CAST(d.date_entered_appt_scheduled as DATE) as appt_date
   , d.is_buyer
   , d.date_deal_created
   , l.id_contact
+  , l.name_owner as contact_owner
   , d.name_owner as deal_owner
-  , l.name_owner as lead_owner
-  , l.name_setter as lead_setter
+  , l.name_setter as contact_setter
   , d.name_setter as deal_setter
+  , l.name_closer as contact_closer
+  , d.name_closer as deal_closer
 FROM `dbt_tscrivo_hubspot_sales.sales_leads` l
 LEFT JOIN `dbt_tscrivo.fct_hs_deal` d
   on l.id_contact = d.id_contact
